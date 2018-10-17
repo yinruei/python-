e69b69d29c1ba5575fd871f1e53ec6a96c467abc
蘇胤瑞 <yinruei@g.ncu.edu.tw>
2018-10-18 01:30:40 +0800

    finish ch6

--- ch6/loginPM25.py
+++ ch6/loginPM25.py
@@ -8,7 +8,11 @@ browser.maximize_window()
 browser.get(url)
 
 sleep(3)  # 必須加入等待，否則會有誤動作
-browser.find_element_by_link_text("大氣").click()
+# browser.find_element_by_link_text("大氣").click()
+browser.find_element_by_xpath("//a[@href='/Data/Details/ATM00625/']").click()#細懸浮微粒
+
+sleep(3)
+browser.find_element_by_class_name("btm02").click()
 
 sleep(3)  # 必須加入等待，否則會有誤動作
 browser.find_element_by_link_text("JSON").click() # 下載 JSON
