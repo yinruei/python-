9ddca79d7b9a9ffe535cb3c3bd109c84a1a37fbf
蘇胤瑞 <yinruei@g.ncu.edu.tw>
2018-10-26 00:55:16 +0800

    update_catch_typhoon_warning

--- catch_typhoon_warnimg.py
+++ catch_typhoon_warnimg.py
@@ -5,25 +5,44 @@ import time
 
 
 driver = webdriver.Chrome() # 如果你沒有把webdriver放在同一個資料夾中，必須指定位置給他
-driver.get("http://rdc28.cwb.gov.tw/TDB/ntdb/pageControl/ty_warning")
+url = "http://rdc28.cwb.gov.tw/TDB/ntdb/pageControl/ty_warning"
+driver.get(url)
 
-# time.sleep(3) # 等待javascript渲染出來，當然這個部分還有更進階的作法，關鍵字是implicit wait, explicit wait，有興趣可以自己去找
-html = driver.page_source # 取得html文字
-time.sleep(3)
+time.sleep(3) # 等待javascript渲染出來，當然這個部分還有更進階的作法，關鍵字是implicit wait, explicit wait，有興趣可以自己去找
 # driver.find_element_by_xpath("//td[@width='40' and @value='1']").click()
-driver.find_element_by_class_name("typhoon_id").click()
-time.sleep(5)
-title = driver.find_element_by_class_name("tyde_t")
+list1 =driver.find_elements_by_class_name("typhoon_id")
+list1[1].click()
+print(list1[1].text)
+print(len(list1))
+# print(list1[0].text)
+#  time.sleep(5)
+
+
+
+typhoon_info = driver.find_elements_by_class_name("typhoon_detail")
+print(typhoon_info)
+alt = typhoon_info.find_elements_by_css_selector(".alt")
+# print(len(alt))
+# print(alt.text)
+for name_list in alt:
+    print(name_list.text)
+
+time.sleep(2)
+name = driver.find_element_by_css_selector(".alt")
+print(type(name))
+print(name.text)
+
+time.sleep(2)
+# start_time = driver.find_elements_by_xpath("//td[@align='left' and @class='td_title']")
+# print(start_time[4].text)
+
+start_time = driver.find_elements_by_css_selector(".alt")
+print(start_time[3].text)
+# title = driver.find_element_by_class_name("tyde_t")
 # print(title.text)
-# sea_warning  = 
-# land_warning = 
 
-# <td width="70" rowspan="1">山竹</td>
-# typhoon_name = driver.find_elements_by_xpath("//td[@width='70' and @rowspan='1']")
-# print(type(typhoon_name))
-# print(len(typhoon_name))
-
-for name in driver.find_elements_by_xpath("//td[@width='70' and @rowspan='1']"):
-
-    print(name.text)
+# for name in driver.find_elements_by_xpath("//td[@width='70' and @rowspan='1']"):
+#     data = name.text
+#     print(type(data))
+    # print(data)
 driver.close()  # 關掉Driver打開的瀏覽器
