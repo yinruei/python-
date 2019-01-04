82e45645e9908ea701a2feed0995d07ab50ef4b9
yinruei <yinruei@cloudybay.com.tw>
2019-01-04 18:22:02 +0800

    update

--- python_exercise/fuzzbuzz.py
+++ python_exercise/fuzzbuzz.py
@@ -3,34 +3,46 @@
 如果能同時被3 和5 整除，就顯示Fizz Buzz 。結果應該類似：0,1,2 ，Fizz ，4 ，Buzz ，6 …… 14 ，FizzBu​​zz ，16 ……
 '''
 
-# def output(number):
-#     result = []
-#     if number % 3 == 0 or 3:
-#         result.append('Fizz')
-#     elif number % 5 == 0 or 5:
-#         result.append('Fizz')
-# print(output(3))
-
-
-def output(num):
+def output(number):
     result = []
-
-    if num % 3 == 0 or '3' in str(num):
+    if number % 3 == 0 and number % 5 == 0:
+        result.append('Fizz Buzz')
+    elif number % 3 == 0:
         result.append('Fizz')
-    if num % 5 == 0 or '5' in str(num):
+    elif number % 5 == 0:
         result.append('Buzz')
-    if num % 7 == 0 or '7' in str(num):
-        result.append('Whizz')
+    else:
+        return number
+    return ''.join(result)
 
-    if not result:
-        return str(num)
-
-    return "".join(result)
-
-
-def buzzer():
+def sum_num():
     for num in range(1,101):
         print(output(num))
 
 if __name__ == '__main__':
-    buzzer()
+    sum_num()
+
+
+
+# def output(num):
+#     result = []
+
+#     if num % 3 == 0 or '3' in str(num):
+#         result.append('Fizz')
+#     if num % 5 == 0 or '5' in str(num):
+#         result.append('Buzz')
+#     if num % 7 == 0 or '7' in str(num):
+#         result.append('Whizz')
+
+#     if not result:
+#         return str(num)
+
+#     return "".join(result)
+
+
+# def buzzer():
+#     for num in range(1,101):
+#         print(output(num))
+
+# if __name__ == '__main__':
+#     buzzer()
